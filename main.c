05b42d49afd70de944d36cf17a41dc353745f917
Daniil Markevich <epidath@gmail.com>
2022-10-13 21:20:47 +0300

    conditional compilation

--- main.c
+++ main.c
@@ -1,23 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <dirent.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+    #include <windows.h>
+#endif
+
 
 char *pathcat(char *str1, char *str2);
 
 int main(int argc, char *argv[])
 {
-    struct dirent *dp;
-    char *fullpath;
     char *path = argv[1]; // Directory target
-    DIR *dir = opendir(path); // Open the directory - dir contains a pointer to manage the dir
-    while (dp=readdir(dir)) // if dp is null, there's no more content to read
-    {
-        fullpath = pathcat(path, dp->d_name);
-        printf("%s\n", fullpath);
-        free(fullpath);
-    }
-    closedir(dir); // close the handle (pointer)
+    #ifdef __linux__ 
+        #include <dirent.h>
+        struct dirent *dp;
+        char *fullpath;
+        
+        DIR *dir = opendir(path); // Open the directory - dir contains a pointer to manage the dir
+        while (dp=readdir(dir)) // if dp is null, there's no more content to read
+        {
+            fullpath = pathcat(path, dp->d_name);
+            printf("%s\n", fullpath);
+            free(fullpath);
+        }
+        closedir(dir); // close the handle (pointer)
+    #elif defined(_WIN32) || defined(_WIN64)
+        HANDLE fileHandle;
+        WIN32_FIND_DATA ffd;
+        LARGE_INTEGER szDir;
+        WIN32_FIND_DATA fileData;
+        fileHandle = FindFirstFile("C:\\Users\\epida\\Desktop\\markevich\\ff_project\\*.?d", &ffd);
+        if (INVALID_HANDLE_VALUE == fileHandle) 
+            return 1;
+
+        do
+        {
+        printf("%s\n", ffd.cFileName);
+        }
+        while (FindNextFile(fileHandle, &ffd) != 0);
+
+    #endif
+    
     return 0;
 }
 
