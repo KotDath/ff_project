2fd0ecbad58de5ea3f5c5ca053ee095ee4635a4f
Daniil Markevich <epidath@gmail.com>
2022-10-14 21:54:37 +0300

    cmake fix 2

--- recSearch.h
+++ recSearch.h
@@ -1,6 +1,10 @@
 #ifndef RECSEARCH_H
 #define RECSEARCH_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 void WindowsRecSearch(char *path, int pathSize, char *pattern, int offset);
 void LinuxRecSearch(char *path, char *pattern, int offset);
 
